42b635a88c891a02ffc2e7479c0a772763d2ed65
seong954t <seomtae@naver.com>
2018-07-02 12:16:23 +0900

    login_function js update

---
+++ FireKakao/login_func.js
@@ -0,0 +1,59 @@
+// 로딩창을 띄운다.
+function showLoading(){
+    $("#spinner-warpper").show();
+}
+
+// 로딩창을 없앤다.
+function hideLoading(){
+    $("#spinner-warpper").hide();
+}
+
+function getEmail(){
+    return $("#kakao-email").val();
+}
+
+function getPassword(){
+    return $("#kakao-pw").val();
+}
+
+firebase.auth().onAuthStateChanged(function (user) {
+    showLoading();
+    if (user) {
+        location.href = './chat.html'
+    }else{
+        hideLoading();
+    }
+});
+
+function signup(){
+    showLoading()
+    firebase.auth().createUserWithEmailAndPassword(getEmail(), getPassword())
+    .then(
+        function(user){
+            hideLoading();
+        },
+        function(error){
+            if(error.code == "auth/email-already-in-use"){
+                signin();
+                return;
+            }else if(error.code == "auth/invalid-email"){
+                alert("사용가능하지 않은 이메일 형식입니다.")
+            }else if(error.code == "auth/weak-password"){
+                alert("비밀번호가 취약합니다.");
+            }
+            hideLoading();
+        }
+    )
+}
+
+function signin(){
+    firebase.auth().signInWithEmailAndPassword(getEmail(), getPassword())
+    .then(
+        function(success) {
+            hideLoading();
+        },
+        function(error){
+            hideLoading();
+        }
+    );
+}
